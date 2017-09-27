fe7b27dddc6e743f046da69d774a64b079709802
STEFAN RADU TARNOWETZKI - Student <G00316210@gmit.ie>
2017-09-27 10:33:47 +0100

    Updated Databases name on imports cd \wamp64\bin\mysql\mysql5.7.14\bin mysql -u root -p <

--- Lab 2/superheroes_wk2_part2.sql
+++ Lab 2/superheroes_wk2_part2.sql
@@ -15,11 +15,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS `superheroes`;
+DROP DATABASE IF EXISTS `superheroes_2``;
 
-CREATE DATABASE `superheroes`;
+CREATE DATABASE `superheroes_2`;
 
-USE `superheroes`;
+USE `superheroes_2`;
 
 --
 -- Table structure for table `superhero_city_table`
